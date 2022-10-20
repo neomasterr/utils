52f92b313c11b276144bceeac3b8d1b8377bc686
neomasterr <neomasterr@mail.ru>
2022-10-20 10:54:08 +0400

    master initial commit

--- utils.js
+++ utils.js
@@ -45,8 +45,46 @@ export const getElementDataset = ($element) => {
  * @param  {String} html      HTML валидный текст
  * @return {HTMLElement|Array} Элемент или Массив
  */
-export const createElement = function(html) {
+export const createElement = (html) => {
     const template = document.createElement('template');
     template.insertAdjacentHTML('afterbegin', html.trim());
     return template.children.length > 1 ? [...template.children] : template.firstElementChild;
 }
+
+/**
+ * Продолжительность выполнения анимации
+ * @param  {Element} $el  Проверяемый элемент
+ * @param  {String}  name Имя анимируемого свойства
+ * @return {Number}       Время анимации в милисекундах
+ */
+export const getTransitionDuration = ($el, name) => {
+    const transition = (window.getComputedStyle($el).transition || 'all 0s ease 0s').split(',').map(s => s.trim()).reduce((acc, item) => {
+        const [property, duration, timingFunction, delay] = item.split(' ');
+
+        acc[property] = {
+            duration: parseFloat(duration) * 1000,
+            timingFunction,
+            delay: parseFloat(delay) * 1000,
+        }
+
+        return acc;
+    }, {});
+
+    // имя не передано = любой самый долгий переход
+    if (typeof name == 'undefined') {
+        const timings = [];
+        for (const key in transition) {
+            timings.push(transition[key].duration + transition[key].delay);
+        }
+        return Math.max.apply(null, timings);
+    }
+
+    // имя передано = поиск по имени либо "все"
+    if (transition[name]) {
+        return transition[name].duration + transition[name].delay;
+    } else if (transition['all']) {
+        return transition['all'].duration + transition['all'].delay;
+    }
+
+    return 0;
+},
